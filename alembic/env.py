7c408aa2c6d5d1647f89c9cb554a4c496042ed85
User <user@example.com>
2026-02-24 16:03:55 +0530

    add alembic

--- alembic/env.py
+++ alembic/env.py
@@ -67,12 +67,13 @@ def run_migrations_online() -> None:
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        connection=connection,
+        target_metadata=target_metadata,
+        compare_type=True,
+        render_as_batch=True)
 
         with context.begin_transaction():
             context.run_migrations()
-
 
 if context.is_offline_mode():
     run_migrations_offline()
