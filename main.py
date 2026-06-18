fa7adfc40f5cb8d24044e74be6616866b7ee5712
User <py6632388@gmail.com>
2026-06-18 18:46:48 +0530

    mobile branch cors

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://ecommerce-frontend-snowy-two.vercel.app","http://localhost:5173"],
+    allow_origins=["https://ecommerce-frontend-snowy-two.vercel.app","http://localhost:5173","https://ecommerce-frontend-kkayklynr-yashs-projects-2f6e6ad4.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
