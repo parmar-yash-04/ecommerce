b16c588ed427837786c38d256eca2b7e9912d83b
User <py6632388@gmail.com>
2026-06-22 15:17:41 +0530

    duplicate user validation

--- app/api/endpoints/users.py
+++ app/api/endpoints/users.py
@@ -10,6 +10,9 @@ router = APIRouter(prefix="/users", tags=["Users"])
 
 @router.post("/create", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    existing = crud_user.get_user_by_email(db, user.email)
+    if existing:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already exists")
     if not user.terms_accepted:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Terms and conditions must be accepted.")
     return crud_user.create_user(db, user)
